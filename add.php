954ab26185dd7ac73cc1097db2a9c4c3a264c3a4
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-11-09 05:44:27 +0800

    init repo

---
+++ add.php
@@ -0,0 +1,37 @@
+<?php
+// save_employee.php
+include('config.php');
+global $conn;
+$current_date = date('d-M-y');
+
+$Fname = $_POST['Fname'];
+$Lname = $_POST['Lname'];
+$Email = $_POST['Email'];
+$Phone = $_POST['Phone'];
+$JobTitle = $_POST['JobTitle'];
+
+// Finding duplicates
+$sql = "SELECT EmployeeID FROM employees WHERE EmployeeFN = '$Fname' AND EmployeeLN = '$Lname'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 0) {
+    // Checks current EmployeeID
+    $sql = "SELECT MAX(EmployeeID) AS MaxID FROM employees";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $newEmployeeID = $row['MaxID'] + 1;
+
+    $sql = "INSERT INTO employees (EmployeeID, EmployeeFN, EmployeeLN, EmployeeEmail, EmployeePhone, HireDate, ManagerID, JobTitle)
+                VALUES ($newEmployeeID, '$Fname', '$Lname', '$Email', '$Phone', '$current_date', 50, '$JobTitle')";
+
+    if ($conn->query($sql)) {
+        header("location:index.php");
+    } else {
+        echo $conn->error;
+    }
+} else {
+    $error = "The name already exists. Please input another one, or edit the information.";
+    echo $error;
+    header("location:index.php");
+}
+?>
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$user = "seed";
+$pass = "seed";
+$server = "database";
+$dbname = "employees";
+
+//$user = "seed";
+//$pass = "seed";
+//$server = "localhost";
+//$dbname = "employees";
+
+$conn = new mysqli($server, $user, $pass, $dbname);
+if ($conn->connect_error) {
+    die("Connection Error: " . $conn->connect_error);
+}
+?>
